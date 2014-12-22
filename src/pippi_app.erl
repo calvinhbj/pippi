150f52d379b97519852c95ba2188cef56d95b8ed
homeway <homeway.xue@gmail.com>
2014-12-22 14:51:14 +0800

    使用ets存储 pp_model pp_theme pp_db 等模型配置数据

--- src/pippi_app.erl
+++ src/pippi_app.erl
@@ -10,6 +10,13 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    %% 初始化所需的ets表
+    %% 存储命名的模型配置（根据用户配置生成：键为业务模型名称，值为字段描述列表）
+    ets:new(pp_model, [named_table, public]),
+    %% 存储表单样式（如果用户未建立表单样式，则使用默认样式）
+    ets:new(pp_theme, [named_table, public]),
+    %% 存储命名的数据库配置
+    ets:new(pp_db, [named_table, public]),
     pippi_sup:start_link().
 
 stop(_State) ->
