0c573196eff42b91c22c5e0064d78381df5bc508
homeway <homeway.xue@gmail.com>
2014-12-27 14:42:56 +0800

    补充pp_logic的函数调用方法: reconsult, asserts, assertz, retractall等
    重新整理了pp_logic的实现

--- src/pp_logic.erl
+++ src/pp_logic.erl
@@ -11,7 +11,8 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, load/1, assert/1, is_true/1, query/1, querys/1, stop/0]).
+-export([start_link/0, consult/1, reconsult/1, assert/1, asserta/1, assertz/1,
+         retract/1, retractall/1, is_true/1, prove/1, query/1, querys/1, stop/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -33,10 +34,24 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-load(File)      -> gen_server:call(?SERVER, {load, File}).
+%% 加载prolog文件
+consult(File)   -> gen_server:call(?SERVER, {consult, File}).
+%% 重新加载prolog文件
+reconsult(File) -> gen_server:call(?SERVER, {reconsult, File}).
+%% 添加事实
 assert(Assert)  -> gen_server:call(?SERVER, {assert, Assert}).
+asserta(Assert) -> gen_server:call(?SERVER, {asserta, Assert}).
+assertz(Assert) -> gen_server:call(?SERVER, {assertz, Assert}).
+%% 去除事实
+retract(Assert) -> gen_server:call(?SERVER, {retract, Assert}).
+retractall(Assert) -> gen_server:call(?SERVER, {retractall, Assert}).
+%% 判断事实真伪
 is_true(Assert) -> gen_server:call(?SERVER, {is_true, Assert}).
+%% 通用求解
+prove(Assert)   -> gen_server:call(?SERVER, {prove, Assert}).
+%% 查询单个解
 query(Assert)   -> gen_server:call(?SERVER, {query, Assert}).
+%% 查询多个解
 querys(Assert)  -> gen_server:call(?SERVER, {querys, Assert}).
 stop()          -> gen_server:call(?SERVER, stop).
 
@@ -72,31 +87,33 @@ init([]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_call({load, File}, _From, OldState) ->
+handle_call({consult, File}, _From, OldState) ->
     {Code, NewState} = erlog:consult(File, OldState),
-    case Code of
-        ok -> {reply, ok, NewState};
-        _ -> {reply, Code, OldState}
-    end;
+    {reply, Code, NewState};
+handle_call({reconsult, File}, _From, OldState) ->
+    {Code, NewState} = erlog:reconsult(File, OldState),
+    {reply, Code, NewState};
 handle_call({assert, Assert}, _From, OldState) ->
-    %% 避免事实重复
-    case erlog:prove(Assert, OldState) of
-        {{succeed, _}, _} -> {reply, already_exist, OldState};
-        _ ->
-            {Code, NewState} = erlog:prove({assert, Assert}, OldState),
-            {reply, Code, NewState}
-    end;
+    m_assert(Assert, OldState);
+handle_call({asserta, Assert}, _From, OldState) ->
+    m_asserta(Assert, OldState);
+handle_call({assertz, Assert}, _From, OldState) ->
+    m_assertz(Assert, OldState);
+handle_call({retract, Assert}, _From, OldState) ->
+    m_prove({retract, Assert}, OldState);
+handle_call({retractall, Assert}, _From, OldState) ->
+    m_retractall(Assert, OldState);
 handle_call({is_true, Assert}, _From, OldState) ->
-    {{Code, _Result}, _NewState} = erlog:prove(Assert, OldState),
-    case Code of
-        succeed -> {reply, true, OldState};
+    case erlog:prove(Assert, OldState) of
+        {{succeed, _}, _} -> {reply, true, OldState};
         _ -> {reply, false, OldState}
     end;
+handle_call({prove, Assert}, _From, OldState) ->
+    m_prove(Assert, OldState);
 handle_call({query, Assert}, _From, OldState) ->
-    {Result, _} = erlog:prove(Assert, OldState),
-    {reply, Result, OldState};
+    m_query(Assert, OldState);
 handle_call({querys,Assert}, _From, OldState) ->
-    {reply, querys(Assert, OldState), OldState};
+    m_querys(Assert, OldState);
 handle_call(stop, _From, State) ->
     {stop, normal, State}.
 
@@ -155,14 +172,46 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
-querys(Assert, OldState) ->
+m_assert(Assert, OldState) -> m_assert_uni(Assert, assert, OldState).
+m_asserta(Assert, OldState) -> m_assert_uni(Assert, asserta, OldState).
+m_assertz(Assert, OldState) -> m_assert_uni(Assert, assertz, OldState).
+%% m_assert_uni避免了事实重复
+%% 若确实需要增加重复的事实，可使用prove等方法
+m_assert_uni(Assert, Type, OldState) ->
     case erlog:prove(Assert, OldState) of
-        {{succeed, Result}, NewState} ->  querys_acc(NewState, [Result]);
-        {Code, _} -> Code
+        {{succeed, _}, _} -> {reply, already_exist, OldState};
+        _ ->
+            {Code, NewState} = erlog:prove({Type, Assert}, OldState),
+            {reply, Code, NewState}
     end.
-querys_acc(OldState, Acc) ->
+
+%% 递归执行retract，实现retractall功能
+m_retractall(Assert, OldState) ->
+    m_retractall_acc(Assert, OldState, 0).
+m_retractall_acc(Assert, OldState, Acc) ->
+    case erlog:prove({retract, Assert}, OldState) of
+        {{succeed, _}, NewState} -> m_retractall_acc(Assert, NewState, Acc + 1);
+        {fail, _} -> {reply, Acc, OldState};
+        {Code, _} -> {reply, Code, OldState}
+    end.
+
+m_prove(Assert, OldState) ->
+    {Result, NewState} = erlog:prove(Assert, OldState),
+    {reply, Result, NewState}.
+
+m_query(Assert, OldState) ->
+    case erlog:prove(Assert, OldState) of
+        {{succeed, [Result|[]]}, _NewState} ->  {reply, {succeed, Result}, OldState};
+        {Code, _} -> {reply, Code, OldState}
+    end.
+m_querys(Assert, OldState) ->
+    case erlog:prove(Assert, OldState) of
+        {{succeed, [Result|[]]}, NewState} ->  {reply, {succeed, m_querys_acc(NewState, [Result])}, OldState};
+        {Code, _} -> {reply, Code, OldState}
+    end.
+m_querys_acc(OldState, Acc) ->
     case erlog:next_solution(OldState) of
-        {{succeed, Result}, NewState} ->
-            querys_acc(NewState, [Result|Acc]);
+        {{succeed, [Result|[]]}, NewState} ->
+            m_querys_acc(NewState, [Result|Acc]);
         _ -> Acc
     end.
